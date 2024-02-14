d6a66210a1ca85e3b0204e4e0f9c618fb9ea64d6
Sikandar Tahir <159277794+SikandarTahir23@users.noreply.github.com>
2024-02-15 01:43:21 +0500

    Typescript/Javascript 45 Questions
    Typescript/Javascript 45 Questions

---
+++ Q12.ts
@@ -0,0 +1,11 @@
+// 12. Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message
+// should be the same, but each message should be personalized with the person’s name.
+
+let Names: string[] = ["Ali", "Hamza", "Ubair", "Maaz"]
+
+let message: string = "How are You Dear?";
+
+console.log(Names [0] + " " + message)
+console.log(Names [1] + " " + message)
+console.log(Names [2] + " " + message)
+console.log(Names [3] + " " + message)
